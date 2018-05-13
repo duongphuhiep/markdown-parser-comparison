92dbcc40f070ccae10cd23477899e94ba10ca8c0
Phu-Hiep DUONG <duongphuhiep@gmail.com>
2018-05-13 12:45:14 +0200

    enhance test and report

--- README.md
+++ README.md
@@ -8,12 +8,30 @@ As you can see in my `package.json` and `main.js`. I'm comparing
 
 I made them render `sample*.md` files using only default options out of the box.
 
+```
+--./sample1.md--
+markdown-it: 8.192ms
+commonmark: 4.884ms
+marked: 4.929ms
+--
+--./sample2.md--
+markdown-it: 6.713ms
+commonmark: 9.729ms
+marked: 2.826ms
+--
+--./sample3.md--
+markdown-it: 1.692ms
+commonmark: 1.771ms
+marked: 0.946ms
+--
+```
+
 **Conclusion**
 
-* [`markedjs`](https://github.com/markedjs/marked) is the fastest: nearly 2x faster than the others two (no wonder it got far more stars on github).
-* `commonmarkjs` is slightly faster than `markdown-it`.
+* [`markedjs`](https://github.com/markedjs/marked) is the fastest (in plus get many stars on github)
 
 **Disclaimer**
 
 * I used only default options which might not be the most optimize ones for the parsers.
+* The test is executing with nodejs, the result might be greatly different on different browsers.
 * The performance might not the only thing you should look at. For example: if you want to write a markdown editor, with a [preview panel capable of highlighting the current line you are editing](https://raw.githubusercontent.com/duongphuhiep/commonmark-editor/master/commonmark-editor-demo.gif), [`commonmark` can help you](https://github.com/duongphuhiep/commonmark-editor), [`markedjs` cannot](https://github.com/markedjs/marked/issues/1267) (at the moment of writing)
--- main.js
+++ main.js
@@ -3,35 +3,27 @@ var marked = require('marked');
 var mdit = require('markdown-it')();
 var commonmark = require('commonmark');
 
-var sourcePath = "./sample3.md";
+[1, 2, 3].forEach(i => {
+  var sourcePath = "./sample" + i + ".md";
+  var fileContent = fs.readFileSync(sourcePath, 'utf8');
 
-var p = new Promise((resolve, reject) => {
+  console.log('--' + sourcePath + '--');
 
-  fs.readFile(sourcePath, 'utf8', (err, fileContent) => {
-    if (err) {
-      reject(err);
-    }
+  console.time("markdown-it");
+  var result = mdit.render(fileContent);
+  console.timeEnd("markdown-it");
 
-    console.time("markdown-it");
-    var result = mdit.render(fileContent);
-    console.timeEnd("markdown-it");
+  console.time("commonmark");
+  var reader = new commonmark.Parser();
+  var writer = new commonmark.HtmlRenderer();
+  var parsed = reader.parse(fileContent);
+  var result = writer.render(parsed);
+  console.timeEnd("commonmark");
 
-    console.time("marked");
-    var lexed = marked.lexer(fileContent);
-    var parsed = marked.parser(lexed);
-    console.timeEnd("marked");
+  console.time("marked");
+  var lexed = marked.lexer(fileContent);
+  var parsed = marked.parser(lexed);
+  console.timeEnd("marked");
 
-    console.time("commonmark");
-    var reader = new commonmark.Parser();
-    var writer = new commonmark.HtmlRenderer();
-    var parsed = reader.parse(fileContent);
-    var result = writer.render(parsed);
-    console.timeEnd("commonmark");
-
-    resolve(0);
-  });
-})
-
-p.then(function () {
   console.log('--');
-}).catch(console.error);
+});
